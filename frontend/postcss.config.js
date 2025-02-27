22b0ef260305be9434a57f059241970f27844095
whereishikaru <h.suzuki.028@gmail.com>
2025-02-27 22:37:54 +1300

    fix: modify quatation mark and delete korean option

---
+++ frontend/postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: [
+        require('autoprefixer')({
+            overrideBrowserslist: [
+                "last 2 versions",
+                "not dead",
+                "IE 11"
+            ]
+        })
+    ]
+};
