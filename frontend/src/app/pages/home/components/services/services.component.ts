61a45ce5fd26d43aadc9f9ffe66ef6ddbc41af21
MitchellMcMaugh <mitchellmcmaugh@gmail.com>
2026-04-28 19:34:46 +1000

    feat: wire services section heading to Strapi Page serviceText block

--- frontend/src/app/pages/home/components/services/services.component.ts
+++ frontend/src/app/pages/home/components/services/services.component.ts
@@ -3,12 +3,14 @@ import { Component, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 // Components
 import { ServiceCardComponent } from './components/service-card/service-card.component';
 // Services
 import { TranslationHelper } from '../../../../shared/translation-helper';
 import { LanguageService } from '../../../../shared/language.service';
+import { PageService } from '../../../../shared/page.service';
+import { IPage } from '../../../../../util/interfaces';
 
 @Component({
   selector: 'app-services',
@@ -20,9 +22,12 @@ import { LanguageService } from '../../../../shared/language.service';
 
 export class ServicesComponent implements OnDestroy {
   currentLanguage: string = 'en';
+  page$!: Observable<IPage | null>;
   private langChangeSubscription!: Subscription;
 
-  constructor(private languageService: LanguageService) { }
+  constructor(private languageService: LanguageService, public pageService: PageService) {
+    this.page$ = this.pageService.getPage('home');
+  }
 
   ngOnInit(): void {
     this.currentLanguage = this.languageService.getCurrentLanguage();
